dc1250aa1d663a6f419298df579a5e0d4b283b29
peppe-quint-boogaard <peppequintb@gmail.com>
2019-02-06 11:24:15 +0100

    get chuck norris quote

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,24 +1,11 @@
-var maleName = "Joost";
-
-var femaleName = "Henk";
-
-const url =
-  "https://love-calculator.p.mashape.com/getPercentage?fname=" +
-  maleName +
-  "&sname=" +
-  femaleName;
+const url = "https://api.chucknorris.io/jokes/random";
 const request = new XMLHttpRequest();
 
 request.open("get", url, true);
-request.setRequestHeader(
-  "X-Mashape-Key",
-  "cpokaPQAoEmshKG26Ib7JwWYib01p1TF2fWjsnI6wNLkBMnA2O"
-);
-
-request.setRequestHeader("Accept", "application/json");
-
-request.onload = function() {
-  console.log(JSON.parse(this.responseText));
-};
-
+request.addEventListener("load", requestListener);
 request.send();
+
+function requestListener() {
+  let chuckQuote = JSON.parse(this.responseText);
+  let quoteText = (document.querySelector("h1").innerHTML = chuckQuote.value);
+}

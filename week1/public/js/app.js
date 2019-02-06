a32fb1237db298bdc905203ee5e08ba45a7ea55a
peppe-quint-boogaard <peppequintb@gmail.com>
2019-02-06 13:27:03 +0100

    pokemon api

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -38,19 +38,44 @@ const countryName = new Promise(function(resolve, reject) {
   request.send();
 });
 
-Promise.all([chuckQuote, countryName]).then(function(data) {
-  randomQuote(data);
-  randomCountry(data);
+const pokemonName = new Promise(function(resolve, reject) {
+  const url = "https://pokeapi.co/api/v2/pokemon/ditto/";
+  const request = new XMLHttpRequest();
+
+  request.open("get", url, true);
+
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      const data = JSON.parse(request.responseText);
+      resolve(data);
+    } else {
+      // We reached our target server, but it returned an error
+      reject("Error Pokemons");
+    }
+  };
+
+  request.send();
+});
+
+Promise.all([chuckQuote, countryName, pokemonName]).then(function(data) {
+  randomQuote(data[0]);
+  randomCountry(data[1]);
+  randomPokemon(data[2]);
 });
 
 function randomQuote(data) {
   const quoteSentence = (document.querySelector(".chuck-quote").innerHTML =
-    "'" + data[0].value + "'");
+    "'" + data.value + "'");
 }
 
 function randomCountry(data) {
-  console.log(data);
-  let i = Math.floor(Math.random() * 250 + 1);
-  let countryName = (document.querySelector(".country-name").innerHTML =
-    data[1][i].name);
+  const i = Math.floor(Math.random() * 250 + 1);
+  const countryName = (document.querySelector(".country-name").innerHTML =
+    data[i].name);
+}
+
+function randomPokemon(data) {
+  const pokemonName = (document.querySelector(".pokemon-name").innerHTML =
+    data.name);
 }
